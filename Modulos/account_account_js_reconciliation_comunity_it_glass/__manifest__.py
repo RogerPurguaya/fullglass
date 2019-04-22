41eb34163db4bb94369ca05aa6291316520e345b
Johan Miranda <johan.miranda@tecsup.edu.pe>
2019-04-22 09:49:43 -0500

    Añadiendo modulo para habilitar opcion de creacion en campos Many2one

---
+++ Modulos/account_account_js_reconciliation_comunity_it_glass/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'ACCOUNT ACCOUNT IT',
+	'category': 'account',
+	'author': 'ITGRUPO-POWER',
+	'depends': ['account_account_js_reconciliation_comunity_it'],
+	'version': '1.0.0',
+	'description':"""
+	CAMPOS DEL ACCOUNT ACCOUNT
+	""",
+	'auto_install': False,
+	'demo': [],
+	'data':	['account_it_view.xml'],
+	'qweb': ['static/account_template_it.xml'],
+	'installable': True
+}
